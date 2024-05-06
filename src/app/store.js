7aedf711be30fc82452e8f717cc20f99d5ee566c
10ansh01 <10ansh.munjal@gmail.com>
2024-05-06 22:57:13 +0530

    add reducers in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import filterReducer from "../reduxSlices/filterOptionsSlice";
+import jobsReducer from "../reduxSlices/jobsSlice";
+import selectFiltersReducer from "../reduxSlices/selectedFiltersSlice";
+import filteredJobsReducer from "../reduxSlices/filteredJobsSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    jobs: jobsReducer,
+    filtersList: filterReducer,
+    selectedFilters: selectFiltersReducer,
+    filteredJobs: filteredJobsReducer,
+  },
 });
